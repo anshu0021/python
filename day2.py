6b5b0298561014d08b069c5f3b78c06a17432d20
anshu0021 <anshukarki785@gmail.com>
2026-08-14 17:24:45 +0545

    first commit

---
+++ day2.py
@@ -0,0 +1,23 @@
+#number=round(3.14159, 1)
+#print(number)
+
+name="anshu"
+print(type(name))
+
+name="anshu"
+print(name.title())
+
+name="anshu"
+print(len(name))
+
+name="anshu karki"
+print(name.strip())
+
+sentance="hello world"
+print(sentance.replace("hello", "python"))
+
+sentance="hello world"
+print("hello" in sentance)
+
+name="anshu"
+print(name[0:3])
